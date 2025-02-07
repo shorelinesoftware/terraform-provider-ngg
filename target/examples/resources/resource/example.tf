08492d82d91326334d44e29d53e5544c2e906644
denisc@shoreline.io <denisc@shoreline.io>
2025-02-07 13:48:15 +0000

    push target commit

---
+++ target/examples/resources/resource/example.tf
@@ -0,0 +1,14 @@
+
+resource "ngg_resource" "books" {
+  name        = "books"
+  description = "Pods with books app."
+  value       = "host | pod | app='bookstore'"
+}
+
+
+resource "ngg_resource" "params_resource" {
+  name        = "params_resource"
+  description = "params_resource"
+  value       = "host | limit=$limit | az=$az"
+  params      = ["limit", "az"]
+}
